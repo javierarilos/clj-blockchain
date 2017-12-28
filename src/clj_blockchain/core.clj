18c74c840288378d9ff15ac9b0f8e3657d2f8e60
Javier Arias <javier.arias@skyscanner.net>
2017-12-28 17:32:04 +0100

    sha256 using java.security.MessageDigest

--- src/clj_blockchain/core.clj
+++ src/clj_blockchain/core.clj
@@ -5,3 +5,10 @@
   "Doing nothing... for the moment."
   [& args]
   (println "Hello, World! ... clj-blockchain"))
+
+(defn sha256 [string]
+  (let
+    [md (java.security.MessageDigest/getInstance "SHA-256")
+     byt (. string getBytes)
+     digest (. md digest byt)]
+    (clojure.string/join (map (fn [_] (Integer/toHexString (bit-and _ 0xFF))) digest))))
--- test/clj_blockchain/core_test.clj
+++ test/clj_blockchain/core_test.clj
@@ -5,3 +5,22 @@
 (deftest a-test
   (testing "Testing about the truth of life."
     (is (= 1 1))))
+
+(deftest hash-is-repeatable
+  (testing "Hashing same thing twice produces same results"
+    (is (= (sha256 "jajaja") (sha256 "jajaja")))))
+
+(deftest hash-is-a-signature
+  (testing "Hashing something produces a signature different to the original thing"
+    (is (not= (sha256 "jajaja") "jajaja"))))
+
+(deftest hash-is-always-the-same
+  (testing "Hashing something produces always the same result"
+    (is (= (sha256 "jajaja") "a887c3cb3de97b3968be5c7fb81ed9e452928db71d66620b7766bf7c5413878"))))
+
+(deftest hash-is-not-reversible
+  (testing "The hash of a hash does not produce the originally hashed thing"
+    (let
+      [jajaja-hash (sha256 "jajaja")
+       jajaja-hash-hash (sha256 jajaja-hash)]
+    (is (not= "jajaja" jajaja-hash-hash)))))
